d329235a6938d6cc10a31980f3f0959c51814e03
katsuya6152 <katsuya.6152@gmail.com>
2025-01-03 03:29:45 +0900

    feat:RPCを導入

--- apps/web/src/app/(authenticated)/cattle/page.tsx
+++ apps/web/src/app/(authenticated)/cattle/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { client } from "@/lib/rpc";
+import { useEffect } from "react";
 
 export default function CattleListPage() {
-  const [cattle, setCattle] = useState();
-
-  const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api` || "";
-
   const fetchCattle = async () => {
-    const res = await fetch(`${API_URL}/cattle`);
+    const res = await client.api.cattle.$get();
+
     const data = await res.json();
-    setCattle(data);
     console.log(data);
   };
 
---
+++ apps/web/src/lib/rpc.ts
@@ -0,0 +1,17 @@
+import { createClient } from "api";
+
+const myFetch = async (input: RequestInfo | URL, init?: RequestInit) => {
+  const res = await fetch(input, init);
+
+  if (res.status === 204) return Response.json({});
+
+  const jsonData = await res.json();
+
+  if (res.status >= 400) {
+    throw Error(jsonData.message ?? "不明なエラー");
+  }
+
+  return Response.json(jsonData);
+};
+
+export const client = createClient("http://localhost:8787");
