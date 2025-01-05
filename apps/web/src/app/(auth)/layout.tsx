769ebeb02bb63e3f03c739948c523598d3a2ca48
katsuya6152 <katsuya.6152@gmail.com>
2025-01-05 15:51:29 +0900

    feat:仮ロゴ追加

---
+++ apps/web/src/app/(auth)/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import Image from "next/image";
+import "../globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Auth",
+  description: "Auth page",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Image
+          src="/logyuu.png"
+          width={120}
+          height={120}
+          alt="logo"
+          className="absolute top-4 left-4"
+        />
+        {children}
+      </body>
+    </html>
+  );
+}
